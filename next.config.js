5807dbb8e6a4bf24acbcc6a3db76784f14355391
fraggy77 <133785082+fraggy77@users.noreply.github.com>
2025-08-08 11:17:02 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    // Verhindert, dass Next.js beim Build versucht, die Seiten vorzurendern
-    appDir: true
-  },
-  output: 'standalone',
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
